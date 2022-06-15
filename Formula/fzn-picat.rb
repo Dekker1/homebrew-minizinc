d80889e4b1d592d18091722089eec9337c2301b8
Jip J. Dekker <jip@dekker.one>
2022-06-15 19:57:45 +1000

    fzn-picat: add initial formula

---
+++ Formula/fzn-picat.rb
@@ -0,0 +1,41 @@
+class FznPicat < Formula
+  desc "FlatZinc solver based on the Picat Language SAT Engine"
+  homepage "http://picat-lang.org"
+  url "https://github.com/nfzhou/fzn_picat/archive/e32c8a0ec02c4ebcfdab3ee53b0f76fca424b79e.tar.gz"
+  version "3.0.2"
+  sha256 "aac52c4853572999be199d2571405b389ea6d1317bc37983d79c597fce6b389e"
+  license "MPL-2.0"
+  head "https://github.com/nfzhou/fzn_picat.git", branch: "main"
+
+  depends_on "picat"
+
+  def install
+    libexec.install "fzn_parser.pi", "fzn_tokenizer.pi", "fzn_picat_sat.pi"
+    (bin / "fzn-picat").write <<~EOS
+      #!/bin/bash
+      TMPDIR=$(mktemp -d -t fzn-picat.XXX) || exit 1
+      trap 'rm -rf "$TMPDIR"' EXIT
+      ln -s #{libexec}/*.pi $TMPDIR
+      #{HOMEBREW_PREFIX}/bin/picat $TMPDIR/fzn_picat_sat.pi "$@"
+    EOS
+
+    inreplace "picat.msc.in" do |s|
+      s.gsub!(/"executable":\s+"[^"]*"/, "\"executable\": \"#{bin}/fzn-picat\"")
+      s.gsub!(/"mznlib":\s+"[^"]*"/, "\"mznlib\": \"#{share}/minizinc/picat\"")
+    end
+
+    (share / "minizinc").mkpath
+    (share / "minizinc/picat").install "mznlib" => "picat"
+
+    (share / "minizinc/solvers").mkpath
+    (share / "minizinc/solvers").install "picat.msc.in" => "picat.msc"
+  end
+
+  test do
+    (testpath/"test.fzn").write <<~EOS
+      var bool: x1;
+      solve satisfy;
+    EOS
+    assert_match "----------", shell_output("#{bin}/fzn-picat test.fzn").strip
+  end
+end
--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Or `brew tap dekker1/minizinc` and then `brew install <formula>`.
 
 - [Chuffed](https://github.com/chuffed/chuffed) - `brew install dekker1/minizinc/chuffed`
 - [JaCoP](https://github.com/radsz/jacop/) - `brew install dekker1/minizinc/jacop`
+- [FZN Picat](https://github.com/nfzhou/fzn_picat) - `brew install dekker1/minizinc/fzn-picat`
+  - Note: this tap contains only the FlatZinc interface to [Picat](http://picat-lang.org), it depends on the Picat package in Hombebrew core.
 
 ## MiniZinc solvers included in Homebrew core:
 
