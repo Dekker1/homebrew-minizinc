ef1c1ace30e66fe746671cc27a552fe8b7bfac0b
Jip J. Dekker <jip@dekker.one>
2022-06-16 16:40:02 +1000

    chuffed: modernize cmake commands

--- Formula/chuffed.rb
+++ Formula/chuffed.rb
@@ -15,10 +15,9 @@ class Chuffed < Formula
   depends_on "cmake" => :build
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "cmake", "--build", ".", "--target", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
