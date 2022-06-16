bb78872f03c24d01a4418939bb503a0a37ef3e63
Jip J. Dekker <jip@dekker.one>
2022-06-16 15:33:02 +1000

    findmus: add 0.7.0 bottle.

--- Formula/findmus.rb
+++ Formula/findmus.rb
@@ -7,6 +7,12 @@ class Findmus < Formula
   license "MPL-2.0"
   head "https://gitlab.com/minizinc/FindMUS.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/dekker1/minizinc"
+    sha256 cellar: :any,                 big_sur:      "f58961e0be08f2c4edcb248b46d2d38813954b9607373a6bb2d29debe20fe706"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ef2af687cfb36e82305261b035005023b201e983e6d8ed514a9ecf9f582badfd"
+  end
+
   depends_on "cmake" => :build
   depends_on "minizinc"
 
