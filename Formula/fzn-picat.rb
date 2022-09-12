5e57b1ca9848fd7b55fd7548e2151ae071b1d37e
Jip J. Dekker <jip@dekker.one>
2022-09-12 11:02:30 +1000

    fzn-picat 3.3.0

--- Formula/fzn-picat.rb
+++ Formula/fzn-picat.rb
@@ -1,9 +1,9 @@
 class FznPicat < Formula
   desc "FlatZinc solver based on the Picat Language SAT Engine"
   homepage "http://picat-lang.org"
-  url "https://github.com/nfzhou/fzn_picat/archive/284705a4069855dfff3d10ce05254d78a640abcb.tar.gz"
-  version "3.2.8"
-  sha256 "41707a31e1634ee0f821d0d8c6c8e7cb4a862a028b6d73bd76f458cc09021033"
+  url "https://github.com/nfzhou/fzn_picat/archive/8d18e1f5b06c9e52c227f7adf91fa9728c83faeb.tar.gz"
+  version "3.3.0"
+  sha256 "8d74006bce80c6caeb302c96270346b4c130d966ac1f5876f95846f8a3b3aa2b"
   license "MPL-2.0"
   head "https://github.com/nfzhou/fzn_picat.git", branch: "main"
 
