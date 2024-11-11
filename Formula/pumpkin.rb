9e41c0b50ad01a72f17f04ac30e112cae5b07977
Jip J. Dekker <jip.dekker@monash.edu>
2024-11-11 06:05:45 +0000

    pumpkin: add 0.1.4 bottle.

--- Formula/pumpkin.rb
+++ Formula/pumpkin.rb
@@ -5,6 +5,13 @@ class Pumpkin < Formula
   sha256 "7c4bb0ce85d456685b1339370d7dd04e3f3c16a989b93b16f66f0aa266fa5d9e"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/dekker1/minizinc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "041dc321da817f1344d9b1d89ad9cb0e92346a0d0a63b128e2d4b1e88d351bbc"
+    sha256 cellar: :any_skip_relocation, ventura:      "544eee75176acf8cea6b654237b253abc767cac288b3353a785fd392143e7f5a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0b1e16f817195068028579c2e85fef297f597b6e37935954c2e703381cee33bb"
+  end
+
   depends_on "rust" => :build
 
   def install
